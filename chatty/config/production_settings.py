9fcb385bb90dc65327be374b568d9482424da3f4
Andreas Cormack <andreas@passthekeys.co.uk>
2024-07-24 19:48:10 +0100

    added google authentication

--- chatty/config/production_settings.py
+++ chatty/config/production_settings.py
@@ -6,7 +6,7 @@ import json
 DEBUG = False
 
 
-ALLOWED_HOSTS = ["ec2-52-207-91-151.compute-1.amazonaws.com",]
+ALLOWED_HOSTS = ["chattymusic.com",]
 
 CSRF_TRUSTED_ORIGINS = [
     'http://ec2-52-207-91-151.compute-1.amazonaws.com',
